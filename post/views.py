d33a90e16024d7946019d3dea146abd8fd6b62a0
edzen12 <oichiev.edzen@gmail.com>
2026-03-26 19:15:30 +0600

    я создал проект для учебного проекта

---
+++ post/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from post.models import Article
+
+
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles':articles
+    }
+    return render(request, 'index.html', context)
