69f959670eaa8b80e76c126566d67baf61b808c1
edzen12 <oichiev.edzen@gmail.com>
2026-03-28 19:20:56 +0600

    edit models and update index.hmtl

--- post/admin.py
+++ post/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from post.models import Article
+from post.models import Article, Category
 
 
-admin.site.register(Article)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'slug']
+    list_display_links = ['id', 'name']
+    prepopulated_fields = {'slug':('name',)}
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'category']
+    list_display_links = ['id', 'title']
+    prepopulated_fields = {'slug':('title',)} 
+
--- post/models.py
+++ post/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Название", unique=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, null=True,
+        related_name="articles", verbose_name="Категория"
+    )
     title = models.CharField(max_length=150)
     description = models.TextField()
     image = models.ImageField(upload_to='posts', null=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
+    slug = models.SlugField(unique=True, null=True)
 
     def __str__(self):
-        return self.title
+        return f"ID:{self.id}  _Название: {self.title}"
