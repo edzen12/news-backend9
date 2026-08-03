cdc93fbce5212137a008e167b4d746f6772aea1f
edzen12 <oichiev.edzen@gmail.com>
2026-08-03 18:51:26 +0600

    staticfiles

--- news/settings.py
+++ news/settings.py
@@ -135,8 +135,8 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
