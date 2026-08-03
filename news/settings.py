f68755406176b3a1a8f1f79e85e321cea591c6de
edzen12 <oichiev.edzen@gmail.com>
2026-08-03 18:29:58 +0600

    edit host

--- news/settings.py
+++ news/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*i^_&5m+i0r4e3t3!mwa)tt2-^)z@n23$#fwjjx-a41w&mwus$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['178.219.159.250']
 
 
 # Application definition
